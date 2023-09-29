a8d2d0759db4f59cceb4efa357e2921a42147d40
Mark Curtiss <markcurtiss720@gmail.com>
2023-09-28 19:45:19 -0600

    file paths in HTML.index were corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
-const url = 'https://epic-free-games.p.rapidapi.com/epic-free-games';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'dbbe26a28cmsh58d0c330cc6e4fcp1d024ajsnff75918c19c3',
-		'X-RapidAPI-Host': 'epic-free-games.p.rapidapi.com'
-	}
-};
+//const url = 'https://epic-free-games.p.rapidapi.com/epic-free-games';
+//onst options = {
+//	method: 'GET',
+//	headers: {
+//		'X-RapidAPI-Key': 'dbbe26a28cmsh58d0c330cc6e4fcp1d024ajsnff75918c19c3',
+//		'X-RapidAPI-Host': 'epic-free-games.p.rapidapi.com'
+//	}
+//};
 
-async function fetchData() {
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-}
+//async function fetchData() {
+//  try {
+//    const response = await fetch(url, options);
+//    const result = await response.text();
+//    console.log(result);
+//  } catch (error) {
+//    console.error(error);
+//  }
+//}
 
-fetchData();
+//fetchData();
 
-window.onload = fetchData;
+//window.onload = fetchData;
