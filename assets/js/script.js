18fbeb1f7a0ce7f32943ad9b929e53e5245b72c8
Mark Curtiss <markcurtiss720@gmail.com>
2023-09-28 20:46:47 -0600

    Created variables for APIs that are better to work with

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,6 @@
 
 //window.onload = fetchData;
 
-
-
-
 var freeGamesUrl = "https://epic-free-games.p.rapidapi.com/epic-free-games?rapidapi-key=dbbe26a28cmsh58d0c330cc6e4fcp1d024ajsnff75918c19c3"
 
 fetch(freeGamesUrl)
@@ -47,7 +44,7 @@ fetch(freeGamesUrl)
 
 
   var test = "https://epic-store-games.p.rapidapi.com/onSale?searchWords=Mount&categories=Games&locale=us&country=us&rapidapi-key=206b08d759mshb06b9e5b9dbd779p1ec90ajsn4b8ad24f5041"
-  
+
   fetch(test)
   .then(function (response) {
     return response.json()
@@ -55,5 +52,3 @@ fetch(freeGamesUrl)
     .then(function (data) {
       console.log(data);
   });
-
-
