487e76d36d0a83b9870fed1e4ea6aec61af43ef5
Jackie Chheng <jackiechheng@Jackies-MacBook-Pro.local>
2023-10-02 19:32:24 -0600

    fixed image for cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,21 @@ var freeGamesUrl = "https://epic-free-games.p.rapidapi.com/epic-free-games?rapid
 var upcomingGamesUrl = "https://epic-free-games.p.rapidapi.com/epic-free-games-coming-soon?rapidapi-key=dbbe26a28cmsh58d0c330cc6e4fcp1d024ajsnff75918c19c3";
 var test = "https://epic-store-games.p.rapidapi.com/onSale?searchWords=Mount&categories=Games&locale=us&country=us&rapidapi-key=206b08d759mshb06b9e5b9dbd779p1ec90ajsn4b8ad24f5041";
 //API's for free games of the week, upcoming free games, and test(Games that are discounted and what not)
+
 var urls = [freeGamesUrl, upcomingGamesUrl, test];
+// Array that contains all APIs
 
 var freeGamesArray = [];
 var upcomingGamesArray = [];
-
 var row = document.getElementById("row");
 
 
-
-
+// Fetches data from multiple APIs
 Promise.all(
   urls.map(url => fetch(url).then(response => response.json()))
 )
 .then(function (dataArray) {
-  console.log("Free Games Data:", dataArray[0]);
+  console.log("Free Games Data:", dataArray[0]); // Logs data from APIs
   for (var i = 0; i < dataArray[0].length; i++) {
     var freeGames = {
       image: dataArray[0][i].offerImageWide,
@@ -29,22 +29,21 @@ Promise.all(
   } 
   console.log("Upcoming Games Data:", dataArray[1]);
   for (var i = 0; i < dataArray[1].length; i++) {
-    var upcomingGames = {
+    var upcomingGame = {
       image: dataArray[1][i].offerImageWide,
       name: dataArray[1][i].name,
       publisher: dataArray[1][i].publisher,
       descr: dataArray[1][i].description,
-      link: dataArray[1][i].appUrl
-    }
-    upcomingGamesArray.push(upcomingGames)
+      link: dataArray[1][i].appUrl,
+    };
+    upcomingGamesArray.push(upcomingGame);
   }
-    console.log("On Sale Games Data:", dataArray[2]);
-    generateFreeGames();
+  console.log("On Sale Games Data:", dataArray[2]);
+  generateFreeGames(); // Call the function here, after processing the data
 })
 .catch(function (error) {
   console.error("Error fetching data:", error);
 });
-  
 
 console.log("below should be new object containing the two free games of the week in an array.")
 console.log(freeGamesArray)
@@ -54,33 +53,37 @@ console.log("below should be new object containing the upcoming games in an arra
 console.log(upcomingGamesArray)
 console.log("========================")
 
-
-//creating a card to put information from arrays into
+// Generates the card for the freegames
 function generateFreeGames() {
-for (var i = 0; i < freeGamesArray.length; i++) {
-  var card = document.createElement("div");
-  card.classList.add("card", "col-sm-12", "col-md-3");
-  card.style.width = "18rem";
+  for (var i = 0; i < freeGamesArray.length; i++) {
+    // HTML elements for card
+    var card = document.createElement("div");
+    card.classList.add("card", "col-sm-12", "col-md-3");
+    card.style.width = "18rem";
+    
     var img = document.createElement("img");
     img.classList.add("card-img-top");
-    img.href = freeGamesArray[i].image;
+    img.src = freeGamesArray[i].image; 
+    
     var cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
-      var h5 = document.createElement("h5");
-      h5.textContent = freeGamesArray[i].name;
-      console.log(h5)
-  
-      var p = document.createElement("p");
-      p.textContent = freeGamesArray[i].descr;
-      var a = document.createElement("a");
-      a.href = freeGamesArray[i].link;
-      a.classList.add("btn", "btn-primary")
-      a.textContent = "Go somewhere"
-  row.appendChild(card)
+    
+    var h5 = document.createElement("h5");
+    h5.textContent = freeGamesArray[i].name;
+    
+    var p = document.createElement("p");
+    p.textContent = freeGamesArray[i].descr;
+    
+    var a = document.createElement("a");
+    a.href = freeGamesArray[i].link;
+    a.classList.add("btn", "btn-primary")
+    a.textContent = "Go somewhere"
+    
+    row.appendChild(card)
     card.appendChild(img);
     card.appendChild(cardBody);
-      cardBody.appendChild(h5);
-      cardBody.appendChild(p);
-      cardBody.appendChild(a);
+    cardBody.appendChild(h5);
+    cardBody.appendChild(p);
+    cardBody.appendChild(a);
   }
 }
